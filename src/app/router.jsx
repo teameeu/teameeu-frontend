7ed3a811ba11928a6dfb23a27fe1e1438db57d2f
cwlee <chanuel0110@gmail.com>
2026-04-24 08:35:35 +0900

    Feat: splash 화면 구현

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MainLayout } from "@/widgets/layout";
 import { HomePage } from "@/pages/home";
+import { SplashPage } from "@/pages/splash";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<SplashPage />} />
         <Route element={<MainLayout />}>
-            <Route path="/" element={<HomePage />} />
             <Route path="/home" element={<HomePage />} />
         </Route>
       </Routes>
---
+++ src/pages/splash/ui/SplashPage.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import "./SplashPage.css"
+
+export const SplashPage = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setTimeout(() => {
+            const splash = document.querySelector('.splash');
+            splash.classList.add('fade-out');
+
+            setTimeout(() => {
+                window.location.href = "/home";
+            }, 3000); // CSS duration과 맞추기
+        }, 3000);
+    }, []);
+
+
+    return (
+        <div className="splash">
+            <main className="splash-content">
+                <img src="./logo.svg" alt="logo" />
+                <p className="typo-display-small subtitle">데이터로 그리는 당신만의 가장 빠른 진로 지도</p>
+            </main>
+        </div>
+    );
+};
