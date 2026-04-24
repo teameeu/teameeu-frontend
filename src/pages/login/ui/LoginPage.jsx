7fe1945628007f0686c66fb5ed273b69614a5786
cwlee <chanuel0110@gmail.com>
2026-04-24 09:24:37 +0900

    Feat: 로그인 페이지 html 컴포넌트 추가

--- src/pages/login/ui/LoginPage.jsx
+++ src/pages/login/ui/LoginPage.jsx
@@ -1,7 +1,26 @@
 export const LoginPage = () => {
     return(
         <div>
-            로그인 페이지
+            <div>
+                <p>웨이모와 함께 진로 지도를 그려요</p>
+                <img src="/logo_login.svg" />
+            </div>
+            <form>
+                <input></input>
+                <input type="password"></input>
+                <button>로그인</button>
+            </form>
+            <div className="row">
+                <hr />
+                <span>또는</span>
+                <hr />
+            </div>
+            <div>
+                <span>회원가입</span>
+                <span>|</span>
+                <span>비밀번호 찾기</span>
+            </div>
+
         </div>
     )
 }
