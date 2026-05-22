dca22702c8d4e15b04df50ec9ea346da01f67abb
cwlee <chanuel0110@gmail.com>
2026-05-22 10:00:08 +0900

    Chore: 스타일 수정

--- src/pages/overview/ui/Section05.js
+++ src/pages/overview/ui/Section05.js
@@ -8,7 +8,7 @@ export const Section05 = () => {
             <h4 style={{color: "var(--color-cyan-900)", marginBottom: "-30px", fontSize: "20px", fontWeight: "700"}}>데이터로 그리는 당신만의 가장 빠른 진로 지도</h4>
             <img src="./img/logo/logo03.svg" alt="logo" style={{ height: '50px', margin: "0" }} />
             <button className="section-01-btn btn-primary" style={{margin: "45px 120px", width: "calc(100% - 240px)"}}>
-                <p>바로 시작하기</p>
+                <p style={{ color: "var(--color-base-000)" }}>바로 시작하기</p>
             </button>
         </div>
     )
