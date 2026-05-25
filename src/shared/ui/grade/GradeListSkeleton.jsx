04c1812e5f3ccdeec13c7d819d7085ad4f4de68c
Minter <adf5858@naver.com>
2026-05-25 16:59:20 +0900

    feat: 성적 테이블 스켈레톤 로더 구현

---
+++ src/shared/ui/grade/GradeListSkeleton.jsx
@@ -0,0 +1,11 @@
+import "@/shared/ui/grade/GradeListSkeleton.css"
+
+export const GradeListSkeleton = () => {
+    return (
+        <div>
+            <div className="grade-skeleton-row"/>
+            <div className="grade-skeleton-row"/>
+            <div className="grade-skeleton-row"/>
+        </div>
+    );
+}
